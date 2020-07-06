9015981a62eebf058bd866b33a8b124a444159bb
creazy <mlodyv111@gmail.com>
2020-07-06 16:36:49 +0200

    //First commit

--- src/main/java/pl/creazy/webapp01/gui/CurrencyExchangeGui.java
+++ src/main/java/pl/creazy/webapp01/gui/CurrencyExchangeGui.java
@@ -67,7 +67,6 @@ public class CurrencyExchangeGui extends VerticalLayout{
         });
 
         exchangeButton.addClickListener(event -> {
-            //todo
             UI.getCurrent().getPage().reload();
         });
     }
